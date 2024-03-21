6525bde2829c32c24d5478ae85fead961149311c
Ruben Valdez Luna <ruben.valdez87@gmail.com>
2024-03-21 16:15:56 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/birthday_website/",
+  base: "/feliz_cupleanos/",
   plugins: [
     vue(),
   ],
